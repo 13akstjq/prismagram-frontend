9a4b865c4311648de408e8179a1e6021d74a08e0
13akstjq <13akstjq@naver.com>
2019-07-03 02:10:54 +0900

    Router.js & Routes success

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
+import AppRouter from "./AppRouter";
 
 export default () => (
-  Glo
   <ThemeProvider theme={Theme}>
-    <GlobalStyles />
+    <>
+      <GlobalStyles />
+      <AppRouter isLoggedIn={!true} />
+    </>
   </ThemeProvider>
 );
