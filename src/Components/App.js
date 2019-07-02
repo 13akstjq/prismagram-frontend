634dcf5bacb0d2c24988e285214e417d6dc8a6c1
13akstjq <13akstjq@naver.com>
2019-07-02 22:53:12 +0900

    GlobalStyles.js & Theme.js

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import GlobalStyles from "../Styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import Theme from "../Styles/Theme";
+
+export default () => (
+  Glo
+  <ThemeProvider theme={Theme}>
+    <GlobalStyles />
+  </ThemeProvider>
+);
---
+++ src/Styles/Theme.js
@@ -0,0 +1,18 @@
+const BOX_BORDER = "border: 1px solid #e6e6e6;";
+const BOX_RADIUS = "4px";
+
+export default {
+  bgColor: "#FAFAFA",
+  blackColor: "#262626",
+  darkGreyColor: "#003569",
+  lightGreyColor: "#999",
+  redColor: "#ED4956",
+  blueColor: "#3897f0",
+  boxBorder: "border: 1px solid #e6e6e6;",
+  boxRadius: "4px",
+  whiteBox: `
+        ${BOX_BORDER},
+        ${BOX_RADIUS},
+        background-color : white
+    `
+};
