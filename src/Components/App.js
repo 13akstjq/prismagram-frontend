da4fe9cece8083615a940f8eacf629af31fdd553
한만섭 <13akstjq@naver.com>
2019-07-03 14:17:58 +0900

    Query finish

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
-import { ApolloProvider } from "react-apollo-hooks";
 import { ThemeProvider } from "styled-components";
 import Theme from "../Styles/Theme";
 import AppRouter from "./AppRouter";
-import Client from "../Apollo/Client";
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo-hooks";
 
-export default () => (
-  <ThemeProvider theme={Theme}>
-    <>
-      <GlobalStyles />
-      <AppRouter isLoggedIn={!true} />
-    </>
-  </ThemeProvider>
-);
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+  return (
+    <ThemeProvider theme={Theme}>
+      <>
+        <GlobalStyles />
+        <AppRouter isLoggedIn={isLoggedIn} />
+      </>
+    </ThemeProvider>
+  );
+};
