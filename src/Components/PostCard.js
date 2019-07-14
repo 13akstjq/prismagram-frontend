6bd0a28fa4e72523f36709e15cd899522a0e4a87
13akstjq <13akstjq@naver.com>
2019-07-14 15:05:58 +0900

    Postcard grid sys

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -1,35 +1,59 @@
 import React from "react";
 import styled from "styled-components";
-import { HeartFull, CommentBubble } from "./Icons";
+import { HeartFull, FillComment } from "./Icons";
 
 const Overlay = styled.div`
+  min-width: 300px;
+  min-height: 300px;
   width: 100%;
   height: 100%;
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   justify-content: center;
   opacity: 0;
-  transition: opacity 0.2s linear;
+  transition: opacity 0.1s linear;
   align-items: center;
-  &:svg {
+  svg {
     fill: white;
   }
 `;
 
 const Container = styled.div`
-  width: 300px;
-  height: 300px;
+  cursor: pointer;
+  min-width: 300px;
+  min-height: 300px;
+  background-size: cover;
   background-image: url(${props => props.url});
   &:hover {
     ${Overlay} {
       opacity: 1;
     }
   }
+  &:nth-child(3) {
+    grid-column: 3/5;
+    /* grid-column-start: 3; */
+    /* grid-column-end: 5; */
+    grid-row: 1/3;
+  }
+  &:nth-child(5) {
+    grid-row: 3/5;
+    grid-column: 1/3;
+  }
 `;
 
-const Info = styled.div``;
+const Info = styled.div`
+  display: flex;
+  align-items: center;
+  color: white;
+  &:first-child {
+    margin-right: 15px;
+  }
+`;
 
-const InfoText = styled.span``;
+const InfoText = styled.span`
+  font-size: 20px;
+  margin-left: 5px;
+`;
 
 export default ({ url, likeCount, commentCount }) => {
   console.log(url);
@@ -41,7 +65,7 @@ export default ({ url, likeCount, commentCount }) => {
           <InfoText>{likeCount}</InfoText>
         </Info>
         <Info>
-          <CommentBubble />
+          <FillComment />
           <InfoText>{commentCount}</InfoText>
         </Info>
       </Overlay>
